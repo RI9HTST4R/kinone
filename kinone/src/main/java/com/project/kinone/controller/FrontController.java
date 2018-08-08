5af9d8f1b315528fb610b766627bbe00ed6d567e
bases <bases@DESKTOP-NQ1GGR3>
2018-08-08 17:15:23 +0900

    -매치 슬라이드 고정-

--- kinone/src/main/java/com/project/kinone/controller/FrontController.java
+++ kinone/src/main/java/com/project/kinone/controller/FrontController.java
@@ -2,8 +2,13 @@ package com.project.kinone.controller;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -36,18 +41,31 @@ public class FrontController {
 	private AdminServiceImpl adminService;
 	
 	@RequestMapping(value="/main.do", method=RequestMethod.GET)
-	public String main(Model model) {
+	public String main(Model model, HttpSession ses) {
 		List<Club> clubList = clubService.getClubList();
 		System.out.println(clubList.toString());
 		String seasoncode = adminService.getTopSeason();
 		
-		// 매치 슬라이드 부분 데이터
-		List<Date> k1MatchDays = matchService.get7MatchDays("K1");
-		System.out.println("K1 리그 매치 날짜 : " + k1MatchDays);
-		HashMap<String, HashMap> k1MatchMapList = matchService.getAllMatchInDay(k1MatchDays, "K1");
-		List<Date> k2MatchDays = matchService.get7MatchDays("K2");
-		System.out.println("K2 리그 매치 날짜 : " + k2MatchDays);
-		HashMap<String, HashMap> k2MatchMapList = matchService.getAllMatchInDay(k2MatchDays, "K2");
+		String whendata = (String)ses.getAttribute("whendata");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+		String d = sdf.format(new Date(System.currentTimeMillis()));
+		System.out.println(d);
+		if(whendata == null || !sdf.format(new Date(System.currentTimeMillis())).equals(d)) {
+		
+			// 매치 슬라이드 부분 데이터
+			List<Date> k1MatchDays = matchService.get7MatchDays("K1");
+			System.out.println("K1 리그 매치 날짜 : " + k1MatchDays);
+			HashMap<String, HashMap> k1MatchMapList = matchService.getAllMatchInDay(k1MatchDays, "K1");
+			List<Date> k2MatchDays = matchService.get7MatchDays("K2");
+			System.out.println("K2 리그 매치 날짜 : " + k2MatchDays);
+			HashMap<String, HashMap> k2MatchMapList = matchService.getAllMatchInDay(k2MatchDays, "K2");
+			
+			ses.setAttribute("whendata", d);
+			ses.setAttribute("k1MatchDays", k1MatchDays);
+			ses.setAttribute("k1MatchMapList", k1MatchMapList);
+			ses.setAttribute("k2MatchDays", k2MatchDays);
+			ses.setAttribute("k2MatchMapList", k2MatchMapList);
+		}
 		
 		// 리그 별 클럽 순위 리스트
 		List<Club_season> k1ClubSeasonRankList = clubService.getClubSeasonRankList(seasoncode,"K1");
@@ -62,10 +80,6 @@ public class FrontController {
 		List<Player> k2PlayerSeasonARankList = playerService.getPlayerSeasonRankList(seasoncode,"K2","a");
 		
 		model.addAttribute("clubList", clubList);
-		model.addAttribute("k1MatchDays", k1MatchDays);
-		model.addAttribute("k1MatchMapList", k1MatchMapList);
-		model.addAttribute("k2MatchDays", k2MatchDays);
-		model.addAttribute("k2MatchMapList", k2MatchMapList);
 		model.addAttribute("k1ClubSeasonRankList", k1ClubSeasonRankList);
 		model.addAttribute("k2ClubSeasonRankList", k2ClubSeasonRankList);
 		model.addAttribute("k1PlayerSeasonGRankList", k1PlayerSeasonGRankList);
